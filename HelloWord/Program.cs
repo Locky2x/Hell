31a903e3faf4c7128c25aadfd9cadbe14b79621f
valeramaritin@gmail.com <valeramaritin@gmail.com>
2019-04-23 12:41:54 +0300

    added helloword2

--- HelloWord/Program.cs
+++ HelloWord/Program.cs
@@ -11,6 +11,7 @@ namespace HelloWord
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Word");
+            Console.WriteLine("Hello Word2");
         }
     }
 }
